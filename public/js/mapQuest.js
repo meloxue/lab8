b65f52ca9b5c474a663a19a3a87bb3bdada0c5d1
meloxue <melodyxue1997@gmail.com>
2019-02-27 15:48:21 -0800

    finish lab 8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'fVy70CjLl2N1QAixKAKik4wvAr6GWlPB';
+
+	var map = L. mapquest.map('map', {
+		center: [32.88, -117.238],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.88, -117.238]).addTo(map);
 }
